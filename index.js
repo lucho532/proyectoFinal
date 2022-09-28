d88f469177a68f651bf1603d6b63a7041f35ad87
lucho <luisrak8@gmail.com>
2022-09-28 15:52:40 +0200

    feat: instaled cors

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 require("dotenv").config();
 const express = require("express");
+const cors = require("cors");
 const cookieParser = require("cookie-parser");
 const errors = require("./errors/commons");
 const { PORT } = require("./environments");
@@ -9,6 +10,7 @@ const main = async () => {
   const app = express();
 
   app.use(express.json());
+  app.use(cors(options));
   app.use(cookieParser());
   app.use("/", require("./services")(db));
 
